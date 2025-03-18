4a65e13bb667b03ee99c0512c44f66374ae7dd3a
mathunder30 <mateustlopes11@gmail.com>
2025-03-18 20:22:53 -0300

    Criação: configurando server

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -7,13 +7,18 @@ import path from 'path';
 const app = express();
 const port = 3000;
 import router from './routes/Routes';
+import routes from "./routes/Routes";
 
 app.use(bodyParser.urlencoded({extended: true}));
+
+// acesso as rotas
+app.use('api/financas', routes);
 
 app.use(express.json());
 app.use(cors());
 dotenv.config();
 
+// Teste para conectar com banco de Dados
 promisePool.getConnection()
     .then((connection) => {
         console.log('Conectado ao Banco de Dados');
@@ -24,6 +29,7 @@ promisePool.getConnection()
         process.exit(1);
     });
 
+//acessando servidor!!!
 app.listen(port, () => {
     console.log(`Servidor rodando em http://localhost:${port}`);
 });
